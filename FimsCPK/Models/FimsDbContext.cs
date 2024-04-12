26a50a2eb7b393568423932eef13376d75f18175
ys.cho <ys.cho@fstc.co.kr>
2024-04-12 12:53:25 +0900

    모델 스펙을 표시할 때 채널 수만큼만 표시

--- FimsCPK/Models/FimsDbContext.cs
+++ FimsCPK/Models/FimsDbContext.cs
@@ -32,7 +32,7 @@ public partial class FimsDbContext : DbContext
             .AddJsonFile("appsettings.json")
             .Build();
 
-        optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnectionDevelop"));
+        optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnectionInside"));
     }
     /*
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
