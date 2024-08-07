ce3a475c5d29f3cf4f71588d2352f805667283b2
ys.cho <ys.cho@fstc.co.kr>
2024-08-07 16:47:16 +0900

    1. v0.9.3 검사수 10개 이상인 것들에 대해 CPK 계산 가능하게 함 2. cpk 계산이 안되는 경우 (모든 검사값이 같은 경우) 정규분포 그래프    그리기 불가하게 함. 3. cpk item이 정의 안된 경우 (ucl/lcl이 없는 경우. cpkitem db가 없는    경우) ucl/lcl을 강제로 정의하여 표시하게 함. 4. 모델의 정확한 명칭이 일치하지 않는 경우 (CS302V1, CS302(aaa) 등),    UCL/LCL을 가져오지 못할 수 있음 ==> CpkItems에 등록 해줘야 한다.

--- FimsCPK/Constants.cs
+++ FimsCPK/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string FimsCpkVersion = "0.9.2";
+        public const string FimsCpkVersion = "0.9.3";
         public const string FimsCpkTitle = "FST FIMS CPK Management System";
     }
 }
