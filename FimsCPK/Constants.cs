ad86cdf1a8af2844305d9184afa874df272029e7
ys.cho <ys.cho@fstc.co.kr>
2025-06-24 01:49:30 +0900

    v0.9.91. Excel=/tmp/FimsSpec.xlsx

--- FimsCPK/Constants.cs
+++ FimsCPK/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string FimsCpkVersion = "0.9.9";
+        public const string FimsCpkVersion = "0.9.91";
         public const string FimsCpkTitle = "FST FIMS CPK Management System";
     }
 }
